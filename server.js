5742ba2a67f86f7518df2634bab6d2427691a23a
Bryan Paez <bryanpaez.125@gmail.com>
2023-02-09 00:53:12 -0600

    [UPDATE] se actualizo el texto

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer(function(req, res) {
-  data="XD"
+  data="<h1>Puto el que lo lea :v</h1>"
   res.writeHead(200, { "Content-Type": "text/html" });
   res.write(data);
   res.end();
